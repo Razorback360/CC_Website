7d64e614b1ff10b76ca835aad1a08f70d18805a2
SauceX <mahmoudzak4@gmail.com>
2023-11-14 09:26:57 +0300

    refactor: move icons file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Icons } from "../components/ui/icons";
+import { Icons } from "./icons";
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 
