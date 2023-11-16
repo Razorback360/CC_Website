e854c4eab6800ce2fd90bc1c658650f5f3896d8e
SauceX <mahmoudzak4@gmail.com>
2023-11-16 16:54:39 +0300

    fix: "back" goes to landing page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -11,7 +11,7 @@ function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <Button
-        onClick={() => router.back()}
+        onClick={() => router.push("/")}
         variant="outline"
         className={cn(
           buttonVariants({ variant: "ghost" }),
