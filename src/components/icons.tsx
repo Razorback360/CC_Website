ae2c4c60549a9fb03851c0e60b3bf98a04668f61
SauceX <mahmoudzak4@gmail.com>
2023-11-16 15:30:04 +0300

    chore: added Microsoft icon to icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -49,6 +49,23 @@ export const Icons = {
   sun: SunMedium,
   moon: Moon,
   laptop: Laptop,
+  microsoft: (props: IconProps) => (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fab"
+      data-icon="microsoft"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-microsoft"
+      viewBox="0 0 16 16"
+      {...props}
+    >
+      {" "}
+      <path d="M7.462 0H0v7.19h7.462V0zM16 0H8.538v7.19H16V0zM7.462 8.211H0V16h7.462V8.211zm8.538 0H8.538V16H16V8.211z" />{" "}
+    </svg>
+  ),
   gitHub: (props: IconProps) => (
     <svg
       aria-hidden="true"
