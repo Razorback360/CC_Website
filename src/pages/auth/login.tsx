f1cc5b3d4de788c56a88d564d712ea8a0678e8fd
SauceX <mahmoudzak4@gmail.com>
2023-11-16 15:35:24 +0300

    feat: back button

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/core/user-auth-form";
 import { Icons } from "@/components/icons";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/router";
 
 function LoginPage() {
+  const router = useRouter();
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <Link
-        href="/home"
+      <Button
+        onClick={() => router.back()}
+        variant="outline"
         className={cn(
           buttonVariants({ variant: "ghost" }),
           "absolute left-4 top-4 md:left-8 md:top-8",
@@ -18,7 +22,7 @@ function LoginPage() {
           <Icons.chevronLeft className="mr-2 h-4 w-4" />
           Back
         </>
-      </Link>
+      </Button>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <Icons.logo className="mx-auto h-6 w-6" />
