e14f2df2821d7f1114cf8c5cfbd5bfece84f73df
SauceX <mahmoudzak4@gmail.com>
2023-11-16 15:31:13 +0300

    chore: placeholder login and register pages

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+function LoginPage() {
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  // custom props here...
+};
+
+const RegisterPage = (props: Props) => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
