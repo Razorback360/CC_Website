b5eb1d22d1de1886191e99c3d51769466c8d534d
SauceX <mahmoudzak4@gmail.com>
2023-11-16 16:29:28 +0300

    fix: change logo to svg

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { UserNav } from "./user-nav";
 import { useSession } from "next-auth/react";
+import { Icons } from "@/components/icons";
 
 type Props = {
   // ...
@@ -15,22 +16,17 @@ const SiteHeader = (props: Props) => {
     <nav className="w-full top-0 relative flex flex-row items-center pe-4">
       {/* socials */}
       {/* main nav */}
-      <div className="flex flex-row justify-between items-center w-full px-2 sm:w-2/3 sm:mx-auto">
+      <div className="flex flex-row justify-between items-center w-full py-4 px-2 sm:w-2/3 sm:mx-auto">
         {/* <Image
               priority
               quality={100}
-              src="/cc-kfupm-logo.png"
+              src="/cc-kfupm-logo.svg"
               fill={true}
               sizes="24rem"
               placeholder="empty"
               alt="kfupm campus at its finest"
             /> */}
-        <img
-          className="w-16 aspect-square"
-          src="/cc-kfupm-logo.png"
-          placeholder="empty"
-          alt="kfupm campus at its finest"
-        />
+        <Icons.logo className="w-20" />
         <ul className="flex gap-2 items-center justify-center">
           <Button variant="link">
             <Link href="...">About Us</Link>
