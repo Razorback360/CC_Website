6253f2e3f3815e5f49bf201f3d601b9d815e3fa5
SauceX <mahmoudzak4@gmail.com>
2023-11-16 16:32:18 +0300

    fix: use correct logo sizing

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -25,7 +25,7 @@ function LoginPage() {
       </Button>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <Icons.logo className="mx-auto h-6 w-6" />
+          <Icons.logo className="mx-auto my-4 w-72" />
           <h1 className="text-3xl font-semibold tracking-tight">Welcome</h1>
           <p className="text-sm text-muted-foreground">
             Enter your username to sign in to your account
