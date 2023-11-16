57ef5cf8f5fef1e8b83ac59dca684547e94528c5
SauceX <mahmoudzak4@gmail.com>
2023-11-16 15:29:45 +0300

    chore: add a simple signin button in footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -4,54 +4,72 @@ import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { useRouter } from "next/router";
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+  const router = useRouter();
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24  md:py-0">
+        {/* already a member section with a signin button */}
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
+            Already a member?{" "}
             <a
-              href={siteConfig.links.twitter}
+              onClick={async () => await router.push("/auth/login")}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              shadcn
+              Sign in
             </a>
-            . Hosted on{" "}
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . Illustrations by{" "}
-            <a
-              href="https://popsy.co"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Popsy
-            </a>
-            . The source code is available on{" "}
-            <a
-              href={siteConfig.links.youtube}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
           </p>
         </div>
-        <ThemeToggle />
+        <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+          <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+            <Icons.logo />
+            <p className="text-center text-sm leading-loose md:text-left">
+              Built by{" "}
+              <a
+                href={siteConfig.links.twitter}
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                shadcn
+              </a>
+              . Hosted on{" "}
+              <a
+                href="https://vercel.com"
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                Vercel
+              </a>
+              . Illustrations by{" "}
+              <a
+                href="https://popsy.co"
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                Popsy
+              </a>
+              . The source code is available on{" "}
+              <a
+                href={siteConfig.links.youtube}
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                GitHub
+              </a>
+              .
+            </p>
+          </div>
+          <ThemeToggle />
+        </div>
       </div>
     </footer>
   );
