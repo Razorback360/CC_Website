fabc30ff1bd51a819c5713ea3c04282757fb99f2
SauceX <mahmoudzak4@gmail.com>
2023-11-16 15:40:55 +0300

    fix: exclude header/footer from a list of routes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import SiteHeader from "./site-header";
 import { SiteFooter } from "./site-footer";
+import { useRouter } from "next/router";
 
 type Props = {
   children: React.ReactNode;
 };
 
+// routes that should not have the header or footer
+const HeaderFooterExclusionRoutes = ["/auth/login", "/auth/register"];
+
 const AppLayout = ({ children, ...props }: Props) => {
+  const { pathname } = useRouter();
   return (
     <div className="flex min-h-full flex-col items-center justify-center">
-      <SiteHeader />
+      {/* exclude header and footer from specified list of pages */}
+      {!HeaderFooterExclusionRoutes.includes(pathname) && <SiteHeader />}
       {children}
-      <SiteFooter />
+      {!HeaderFooterExclusionRoutes.includes(pathname) && <SiteFooter />}
     </div>
   );
 };
