980107e5c7f1e214059c471256e8295e98d35ed6
Abdulellah <aaalamer22@gmail.com>
2023-11-20 09:56:19 +0300

    simplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,65 @@ export default function Home() {
       description: "This is test image 2",
     },
   ];
+  // the data is for rendering the events and members cards
+  const data = {
+    events: [
+      {
+        name: "Event Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        content:
+          "Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum dolor amet",
+      },
+      {
+        name: "Event Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        content:
+          "Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum dolor amet",
+      },
+      {
+        name: "Event Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        content:
+          "Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum dolor amet",
+      },
+      {
+        name: "Event Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        content:
+          "Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum dolor amet",
+      },
+    ],
+    members: [
+      {
+        name: "Member Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        role: "Leader",
+      },
+      {
+        name: "Member Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        role: "Leader",
+      },
+      {
+        name: "Member Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        role: "Leader",
+      },
+      {
+        name: "Member Name",
+        term: "T231",
+        image: "/landing-page-hero.jpg",
+        role: "Leader",
+      },
+    ],
+  };
 
   return (
     <>
@@ -42,159 +101,49 @@ export default function Home() {
         <section className="justify-center items-center w-full flex flex-col mt-10">
           <h1 className="text-2xl font-bold">Featured Events</h1>
           <div className="flex flex-row w-full h-full items-center justify-center flex-wrap">
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Event Name</p>
-                <p className="text-sm text-muted-foreground">T231</p>
-                <p>
-                  Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum
-                  dolor amet{" "}
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Event Name</p>
-                <p className="text-sm text-muted-foreground">T231</p>
-                <p>
-                  Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum
-                  dolor amet{" "}
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Event Name</p>
-                <p className="text-sm text-muted-foreground">T231</p>
-                <p>
-                  Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum
-                  dolor amet{" "}
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Event Name</p>
-                <p className="text-sm text-muted-foreground">T231</p>
-                <p>
-                  Lorem impsum dolor amet Lorem impsum dolor amet Lorem impsum
-                  dolor amet{" "}
-                </p>
-              </CardContent>
-            </Card>
+            {data.events.map((el, index) => {
+              return (
+                <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]" key={index}>
+                  <CardHeader className="p-4">
+                    <img
+                      src={el.image}
+                      className="w-full object-cover h-40 border border-transparent rounded-lg"
+                    ></img>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-xl">{el.name}</p>
+                    <p className="text-sm text-muted-foreground">{el.term}</p>
+                    <p>{el.content} </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
           </div>
         </section>
         <section className="justify-center items-center w-full flex flex-col mt-10">
           <h1 className="text-2xl font-bold">Featured Members</h1>
           <div className="flex flex-row w-full h-full items-center justify-center flex-wrap">
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Member Name</p>
-                <p className="text-sm text-muted-foreground mb-4">T231</p>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Member Name</p>
-                <p className="text-sm text-muted-foreground mb-4">T231</p>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Member Name</p>
-                <p className="text-sm text-muted-foreground mb-4">T231</p>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-              </CardContent>
-            </Card>
-            <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]">
-              <CardHeader className="p-4">
-                <img
-                  src="/landing-page-hero.jpg"
-                  className="w-full object-cover h-40 border border-transparent rounded-lg"
-                ></img>
-              </CardHeader>
-              <CardContent>
-                <p className="text-xl">Member Name</p>
-                <p className="text-sm text-muted-foreground mb-4">T231</p>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-                <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
-                  Leader
-                </a>
-              </CardContent>
-            </Card>
+            {data.members.map((el, index) => {
+              return (
+                <Card className="w-3/4 md:w-1/5 m-10 h-[32rem]" key={index}>
+                  <CardHeader className="p-4">
+                    <img
+                      src={el.image}
+                      className="w-full object-cover h-40 border border-transparent rounded-lg"
+                    ></img>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-xl">{el.name}</p>
+                    <p className="text-sm text-muted-foreground mb-4">
+                      {el.term}
+                    </p>
+                    <a className="py-1 px-2 text-md rounded-full bg-primary mr-2">
+                      {el.role}
+                    </a>
+                  </CardContent>
+                </Card>
+              );
+            })}
           </div>
         </section>
       </div>
