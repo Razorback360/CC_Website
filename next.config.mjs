c4924fe7e174e0cc4430a3fb8cbb639043ce0c26
SauceX <mahmoudzak4@gmail.com>
2023-11-25 01:33:44 +0300

    config: setup localization wrapper, translation files

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@
  */
 await import("./src/env.mjs");
 
+import i18next from "./next-i18next.config.mjs";
+const { i18n } = i18next;
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -13,10 +16,8 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+
+  i18n: i18n,
 };
 
 export default config;
