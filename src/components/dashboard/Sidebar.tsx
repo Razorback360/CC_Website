58cfa4389cf62080931fa54b50544e9a4e1bd666
Moath Abdelaziz <93261842+Moath-COE@users.noreply.github.com>
2023-11-27 21:43:15 +0300

    initial setup

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
 const Sidebar = () => {
-  return <p className="h-full fixed w-1/3 left-0">Hello, World!</p>;
+  return (
+    <section className="h-full fixed lg:w-1/3">
+      <ul className="font-bold text-4xl text-primary flex flex-col gap-4 p-5">
+        <li className="hover:text-white">
+          <h2>
+            <Link href="...">Overview</Link>
+          </h2>
+        </li>
+        <li className="hover:text-white">
+          <h2>
+            <Link href="...">Events</Link>
+          </h2>
+        </li>
+        <li className="hover:text-white">
+          <h2>
+            <Link href="...">Members</Link>
+          </h2>
+        </li>
+        <li className="hover:text-white">
+          <h2>
+            <Link href="...">Privileges</Link>
+          </h2>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Sidebar;
--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -73,7 +73,8 @@ const membersList = [
 
 const Members = () => {
   return (
-    <div className="w-full lg:container lg:py-5">
+    <div className="w-full lg:container lg:py-5 relative">
+      <Sidebar />
       <section className="ml-auto lg:w-2/3 flex flex-col gap-4 px-5 pt-5">
         <div className="flex items-center justify-end gap-4">
           <Button variant="default">
