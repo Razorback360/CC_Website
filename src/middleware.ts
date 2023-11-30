31687cb0678666f92c041419bbf2874567830ad2
SauceX <mahmoudzak4@gmail.com>
2023-11-30 09:28:15 +0300

    fix?: use full url for redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ export function middleware(req: NextRequest) {
   )
     return NextResponse.next();
   if (env.NODE_ENV === "production" && !pathname.includes("/linktree")) {
-    return NextResponse.rewrite("/linktree");
+    return NextResponse.redirect(new URL("/linktree", req.nextUrl));
   }
   return NextResponse.next();
 }
