30f4b3b1130e87658cdf70b6d21b3212ff91bc63
SauceX <mahmoudzak4@gmail.com>
2023-11-30 13:54:43 +0300

    fix: update whatsapp group link

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig: SiteConfig = {
     "A informative website for the Computer Club at King Fahd University of Petroleum and Minerals",
   url: "https://cc-kfupm.netlify.app/", // TODO change this to the actual url
   links: {
-    whatsapp: "https://chat.whatsapp.com/Bu2PzBFZfRC13rAQKsk0oA",
+    whatsapp: "https://chat.whatsapp.com/ELV7zQL14wL6ItapCKOL8b",
     twitter: "https://twitter.com/kfupm_cc",
     linkedin: "https://linkedin.com/company/kfupmcclub/",
     youtube: "https://www.youtube.com/channel/UC53CPMNo35_d3q3QV7Hzu8w",
