fafafc14391144fff52b571ae759374c47886818
SauceX <mahmoudzak4@gmail.com>
2024-01-14 23:12:54 +0300

    chore: reset form on close

--- src/components/popups/AddMemberPopup.tsx
+++ src/components/popups/AddMemberPopup.tsx
@@ -66,7 +66,7 @@ export default function AddNewMemberFormDialog({
   }
 
   return (
-    <Dialog {...props}>
+    <Dialog {...props} onOpenChange={(open) => !open && form.reset()}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="">
         <DialogHeader>
