03e32b174ecca72dff9f2cb9c2b555326bbfbb8f
SauceX <mahmoudzak4@gmail.com>
2024-01-15 22:15:02 +0300

    fix: router name from post to event

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: eventRouter,
+  event: eventRouter,
 });
 
 // export type definition of API
