8504dd3bd9c0ed2a2df3be0691817bbeee8571b3
SauceX <mahmoudzak4@gmail.com>
2024-01-20 21:29:28 +0300

    chore: add system updates router

---
+++ src/server/api/routers/system.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { SystemUpdateType } from "@prisma/client";
+
+// following is the schema for the system update model
+// enum SystemUpdateType {
+//     EVENT_CREATE
+//     EVENT_UPDATE // title/description/date/category/semester
+//     EVENT_DELETE
+//     EVENT_IMAGES_ADD
+//     EVENT_IMAGES_DELETE
+//     EVENT_DOCUMENTATION_ADD
+//     EVENT_DOCUMENTATION_DELETE
+//     MEMBER_ADD
+//     MEMBER_UPDATE // enable/disable/role/tags
+//     MEMBER_DELETE
+// }
+
+// model SystemUpdate {
+//     id          String           @id @default(cuid())
+//     type        SystemUpdateType
+//     description String           @db.Text
+//     date        DateTime         @default(now())
+
+//     // Relation to the user who made the update
+//     author   User   @relation(fields: [authorId], references: [id])
+//     authorId String
+// }
+
+export const systemRouter = createTRPCRouter({
+  getSystemUpdates: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.systemUpdate.findMany({
+      orderBy: {
+        date: "desc",
+      },
+    });
+  }),
+  createSystemUpdate: protectedProcedure
+    .input(
+      z.object({
+        type: z.nativeEnum(SystemUpdateType),
+        description: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.systemUpdate.create({
+        data: {
+          type: input.type,
+          description: input.description,
+          Author: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+});
