f29b24ca7181422bafd17f35bb98e1ed9baba77c
SauceX <mahmoudzak4@gmail.com>
2024-01-21 22:01:33 +0300

    chore: add sidebar members  link

--- src/pages/dashboard/members.tsx
+++ src/pages/dashboard/members.tsx
@@ -4,6 +4,8 @@ import Sidebar from "@/components/dashboard/Sidebar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Member } from "types";
+import DashboardLayout from "@/components/dashboard/dashboard-layout";
+import DashboardMembers from "@/components/dashboard/members/dashboard-members";
 
 const membersList = [
   {
@@ -73,26 +75,36 @@ const membersList = [
 
 const Members = () => {
   return (
-    <div className="w-full lg:container lg:py-5 relative">
-      <Sidebar />
-      <section className="ml-auto lg:w-3/4 flex flex-col gap-4 px-5 pt-5">
-        <div className="flex items-center justify-end gap-4">
-          <Button variant="default">
-            <Link href="...">Add</Link>
-          </Button>
-          <Button variant="secondary">
-            <Link href="...">Edit</Link>
-          </Button>
-          <Icons.search />
-        </div>
-        <div className="flex flex-col gap-4">
-          {membersList.map((member) => (
-            <MemberCard key={member.id} info={member} />
-          ))}
-        </div>
-      </section>
-    </div>
+    <DashboardLayout
+      pageTitle="Members Dashboard"
+      description="General overview of your club."
+      defaultLayout={[265, 440, 655]}
+      navCollapsedSize={0}
+    >
+      <DashboardMembers defaultLayout={[265, 440, 655]} />
+    </DashboardLayout>
   );
 };
 
 export default Members;
+
+{
+  /* <Sidebar />
+  <section className="ml-auto lg:w-3/4 flex flex-col gap-4 px-5 pt-5">
+    <div className="flex items-center justify-end gap-4">
+      <Button variant="default">
+        <Link href="...">Add</Link>
+      </Button>
+      <Button variant="secondary">
+        <Link href="...">Edit</Link>
+      </Button>
+      <Icons.search />
+    </div>
+    <div className="flex flex-col gap-4">
+      {membersList.map((member) => (
+        <MemberCard key={member.id} info={member} />
+      ))}
+    </div>
+  </section>
+</DashboardLayout> */
+}
