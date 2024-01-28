547054f4f931465c0f14ce72d8ec675cf84b0e8c
SauceX <mahmoudzak4@gmail.com>
2024-01-28 23:57:53 +0300

    chore: floating navbar

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -5,6 +5,7 @@ import { UserNav } from "./user-nav";
 import { useSession } from "next-auth/react";
 import { Icons } from "@/components/icons";
 import { useRouter } from "next/router";
+import { cn } from "@/lib/utils";
 
 type Props = {
   // ...
@@ -15,27 +16,33 @@ const SiteHeader = (props: Props) => {
   const router = useRouter();
 
   return (
-    <nav className="w-full top-0 relative flex flex-row items-center pe-4">
-      {/* socials */}
-      {/* main nav */}
-      <div className="flex flex-row justify-between items-center w-full py-4 px-2 sm:w-2/3 sm:mx-auto">
-        <Icons.logo className="w-20" />
-        <ul className="flex gap-2 items-center justify-center">
-          <Button variant="link">
-            <Link href="...">About Us</Link>
-          </Button>
-          <Button variant="link">
-            <Link href="...">Contact</Link>
-          </Button>
-          <Button variant="link">
-            <Link href="...">Events</Link>
-          </Button>
-          <Button variant="default">
-            <Link href="...">Join Us</Link>
-          </Button>
-        </ul>
-      </div>
-      {!!session?.user && <UserNav />}
+    <nav
+      className={cn(
+        "flex justify-between items-center lg:text-xl md:text-lg px-6 py-2 hover:px-7 bg-background outline outline-2 outline-border hover:outline-none hover:bg-accent",
+        "select-none h-fit z-50 transition-all duration-100 shadow-lg fixed inset-x-0 mx-auto w-fit top-4 rounded-lg ",
+      )}
+    >
+      <Icons.logo className="h-16 w-16 cursor-pointer mr-4" />
+      <ul
+        className={cn(
+          "flex flex-row gap-2 flex-shrink-0 items-center text-center justify-end",
+          // sideMenuOpen ? "active" : "",
+        )}
+      >
+        <Button variant="link">
+          <Link href="...">About Us</Link>
+        </Button>
+        <Button variant="link">
+          <Link href="...">Contact</Link>
+        </Button>
+        <Button variant="link">
+          <Link href="...">Events</Link>
+        </Button>
+        <Button variant="default">
+          <Link href="...">Join Us</Link>
+        </Button>
+        {!!session?.user && <UserNav />}
+      </ul>
     </nav>
   );
 };
