0f3e9f6a37d14f1ebf2847fdb3b895fddcb442a7
SauceX <mahmoudzak4@gmail.com>
2024-02-04 18:54:44 +0300

    chore: working event page

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,16 +1,41 @@
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { LayoutGrid } from "@/components/ui/layout-grid";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 import { api } from "@/utils/api";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 export default function Event() {
   const router = useRouter();
   const { id: eventId } = router.query as { id: string };
-  const event = api.event.get.useQuery({ id: eventId });
+  const { data: event } = api.event.get.useQuery({ id: eventId });
+
+  const backgroundColors = ["#0f172a", "#171717", "#2d3748", "#1a202c"];
+  const linearGradients = [
+    "linear-gradient(to bottom right, #06b6d4, #10b981)",
+    "linear-gradient(to bottom right, #ec4899, #6366f1)",
+    "linear-gradient(to bottom right, #f97316, #eab308)",
+    "linear-gradient(to bottom right, #f59e0b, #84cc16)",
+    "linear-gradient(to bottom right, #f472b6, #7c3aed)",
+    "linear-gradient(to bottom right, #ff0000, #00ff00)",
+    "linear-gradient(to bottom right, #0000ff, #ffff00)",
+    "linear-gradient(to bottom right, #ff00ff, #00ffff)",
+  ];
+
+  const randomGradient = useMemo(
+    () => linearGradients[Math.floor(Math.random() * linearGradients.length)],
+    [],
+  );
+
+  if (!event) {
+    // TODO event does not exist (not found page)
+    return <Skeleton />;
+  }
 
   return (
     <>
@@ -18,48 +43,59 @@ export default function Event() {
         <title>Computer Club - KFUPM</title>
         <meta name="description" content="Home of KFUPM's Computer Club" />
       </Head>
-      <div className="w-[99vw]">
-        <section className="flex flex-row mt-5 justify-center">
-          <div className="flex-col flex w-1/6">
-            <img
-              src="https://pbs.twimg.com/media/GFAwUcbWkAAVbkg?format=jpg&name=900x900"
-              className="rounded-xl border-4 border-foreground"
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center w-auto">
-            <h1 className="text-2xl">Lorem Ipsum Dolor</h1>
-            <p className="text-md text-muted-foreground w-1/2 mb-1">
-              {event.data?.date.toDateString()} • Organized by Name 1, Name 2,
-              Name 3
-            </p>
-            <div className="flex flex-row mb-2 space-x-2">
-              <Badge variant="secondary">{event.data?.Category.name}</Badge>
-              <Badge variant="secondary">
-                Term {event.data?.Semester.number}
-              </Badge>
-            </div>
-            <p className="text-md text-muted-foreground w-1/2">
-              Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
-              dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit
-              amet
-            </p>
-            <Button className="w-1/2 my-2" asChild variant="default" size="sm">
-              <Link href="...">Register</Link>
-            </Button>
-          </div>
-        </section>
-        <Separator className="my-5" />
-        <div className="h-screen py-5 w-full flex flex-col justify-center items-center">
-          <h1 className="text-2xl">Image Gallery</h1>
-          <LayoutGrid cards={cards} />
+      <main className="flex justify-between items-start p-10 gap-20 h-screen w-screen">
+        <div
+          style={{
+            background: randomGradient,
+          }}
+          className={cn(
+            "hidden lg:flex flex-col aspect-[19/23] ms-10 flex-shrink-0",
+            // inset y = rest of the height of the screen / 2
+            "2xl:h-[80vh] 2xl:inset-y-[10vh] xl:h-[65vh] xl:inset-y-[17.5vh]",
+            "lg:h-[50vh] lg:inset-y-[25vh]",
+            "p-4 rounded-lg sticky justify-between items-center overflow-hidden ",
+          )}
+        >
+          <Image
+            className="rounded-lg select-none max-h-full overflow-hidden"
+            src={
+              // "https://nfjirfbkulkxtgkdqmtn.supabase.co/storage/v1/object/public/images/Web%20Development%20Bootcamp/poster/GFAwUcbWkAAVbkg.jpg"
+              event.Attachments.filter(
+                (attachment) => attachment.type === "EVENT_POSTER",
+              ).at(0)?.src ?? "/placeholder/event.png"
+            }
+            loading="eager"
+            width={1920}
+            height={1080}
+            layout="responsive"
+            objectFit="contain"
+            objectPosition="start"
+            alt="event image"
+          />
+          <Button
+            className="w-full mt-4 flex-shrink-0"
+            size="xl"
+            variant="outline"
+            asChild
+          >
+            <Link href={event.link}>Register</Link>
+          </Button>
         </div>
-      </div>
+        <div className="flex flex-col items-start max-h-[80vh] my-16 me-20 h-full w-full text-white">
+          <h2 className="text-6xl font-bold"> {event.title}</h2>
+          <p dir="rtl" className="text-3xl h-full w-full mt-20 text-right">
+            {event.description}
+          </p>
+          <Separator className="my-5" />
+          <div className="h-screen py-5 w-full flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Image Gallery</h1>
+            <LayoutGrid cards={cards} />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
-const Skeleton = () => {
-  return <div></div>;
-};
 
 const cards = [
   {
